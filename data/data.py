f98130169f27329f02d3dd4ec47de55ee7a5f215
Pepe DonPPrron <jo.guevara39@gmail.com>
2025-08-04 22:54:06 -0600

    Initial commit

---
+++ data/data.py
@@ -0,0 +1,7 @@
+urban_routes_url = 'https://cnt-a1d9280e-b317-46b3-b66b-5bc5fb06b267.containerhub.tripleten-services.com?lng=es'
+address_from = 'East 2nd Street, 601'
+address_to = '1300 1st St'
+phone_number = '+1 123 123 12 12'
+card_number, card_code = '1234 5678 9100', '111'
+# message_for_driver = 'Llevame al museo'
+message_for_driver = 'Muéstrame el camino al museo'
